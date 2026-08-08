2eae907fc06beda6b7e782f893998d092a358b8e
monia07 <monia_7@hotmail.com>
2026-08-08 04:02:26 +0200

    Create listing form and save listings to database

--- listings/urls.py
+++ listings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.listing_list, name="listing_list"),
+    path("add/", views.add_listing, name="add_listing"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
+from .forms import ListingForm
 
 
 def listing_list(request):
     listings = Listing.objects.all()
+
     return render(request, "listings/listing_list.html", {
         "listings": listings
+    })
+
+
+def add_listing(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("listing_list")
+
+    else:
+        form = ListingForm()
+
+    return render(request, "listings/add_listing.html", {
+        "form": form
     })
